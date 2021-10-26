e98813676297dc15232fa27dcb820ac360235286
Tobias Engli Fladen <tobiae1805@osloskolen.no>
2021-10-26 09:03:11 +0200

    test

--- DNS/dns.py
+++ DNS/dns.py
@@ -1,8 +1,3 @@
-x = 3
-y = -1.5
-m = 17
-n = 4
-s = "hello"
-t = "world"
-
-print (s * 2)
+ord=input("Skriv inn et ord: ")
+bokstaver=len(ord)
+print(ord[0]+""+ord[1]+ " ... "+ord[bokstaver-2]+""+ord[bokstaver-1])
